e15bed2e3d74dc64305e82a2de8c44ab13d985fb
Noah_cn <1845533698@qq.com>
2022-08-25 15:57:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,6 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur1409046-09bfa6fc4779c8a9bc6e57e1',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 日志天数
