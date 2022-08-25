f6ba854966a855518e6c3a76781918471d9d1c52
Noah_cn <1845533698@qq.com>
2022-08-25 15:52:21 +0800

    up

---
+++ config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1409046-09bfa6fc4779c8a9bc6e57e1',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: '#'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yiktt'
+    },
+    {
+      text: 'Blog',
+      url: 'https://1e1.top/'
+    },
+  ],
+};
