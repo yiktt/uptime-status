99094b0f5f4b970d44022a067ea99a40e9b67d55
Noah_cn <1845533698@qq.com>
2022-08-25 16:54:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm792539942-b4e68fe2a8ddcc692d0cd155',
-	'm792540060-6779c1cde71fb7c146ff747d',
-	'm792540118-a8ee603dc7eed45e653ba5be',
+    'm792540060-6779c1cde71fb7c146ff747d',
+    'm792540118-a8ee603dc7eed45e653ba5be',
   ],
 
   // 日志天数
-  CountDays: 60,
+  CountDays: 90,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -33,3 +33,4 @@ window.Config = {
     },
   ],
 };
+
